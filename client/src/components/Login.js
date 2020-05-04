318d68aa25eb0381464b014fe3f4c0ba0610cbf9
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-04 09:23:47 +0800

    completed authentication of app, PrivateRoute built to redirect to protected page after authenticated

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,7 +25,7 @@ class Login extends React.Component {
       .then(res => {
         console.log(res);
         localStorage.setItem("token", res.data.payload);
-        // this.props.history.push("/protected");
+        this.props.history.push("/protected");
       })
       .catch(err => console.log(err));
   };
